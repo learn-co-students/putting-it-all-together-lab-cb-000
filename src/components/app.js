e1b7e8d3afcca7271d463a39855c70cc9050e9e5
Michael Anderson <michanders@msn.com>
2018-03-07 23:40:17 +0000

    Done.

---
+++ src/components/app.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import UserBlackjack from './user_blackjack'
+import AIBlackjack from './ai_blackjack'
+
+import { hitAi, hitUser } from '../actions/blackjack_actions'
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.hitMe = this.hitMe.bind(this)
+    this.stay = this.stay.bind(this)
+    this.calculateAiScore = this.calculateAiScore.bind(this)
+    this.calculateUserScore = this.calculateUserScore.bind(this)
+  }
+
+  hitMe(user){
+    const storeState = this.props.store.getState().deck;
+    user === true ? this.props.store.dispatch(hitUser(storeState)) : this.props.store.dispatch(hitAi(storeState));
+  }
+
+  calculateAiScore(){
+    const ai = this.props.store.getState().aiCards.reduce((accumulator, currentValue) => {return accumulator + currentValue.value }, 0);
+    return (ai > 21 ? "Over 21" : ai)
+  }
+
+  calculateUserScore(){
+    const user = this.props.store.getState().userCards.reduce((accumulator, currentValue) => {return accumulator + currentValue.value }, 0);
+    return (user > 21 ? "Over 21" : user)
+  }
+
+  stay() {
+    const userScore = this.calculateUserScore;
+    const aiScore = this.calculateAiScore;
+    if (userScore !== "Over 21") {
+      while (userScore > aiScore) {
+        return aiScore === "Over 21" ? nil : this.hitMe(false)
+      }
+    }
+  }
+
+  render() {
+    const getUserCards = this.props.store.getState().userCards;
+    const getAiCards = this.props.store.getState().aiCards;
+    return(
+      <div>
+        <UserBlackjack userCards={ getUserCards } hitMe={ this.hitMe } score={ this.calculateUserScore } calculateAiScore={ this.calculateAiScore } stay={this.stay } />
+        <AIBlackjack aiCards={ getAiCards } hitMe={ this.hitMe } score={ this.calculateAiScore } userScore={ this.calculateUserScore } />
+      </div>
+    )
+  }
+}
